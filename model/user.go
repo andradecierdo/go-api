a6586d1ae7d40ac5b28ee3fb218d6c2b5d36612b
Andrade Decierdo <andraded@codev.com>
2024-04-05 02:54:55 +0800

    Create models for user, blog, and media

---
+++ model/user.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	Blogs     []Blog    `json:"blogs"`
+}
